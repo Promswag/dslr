3f5e2f9ce49f30565b83524b5a26a50cfc31fdba
jmathieu <jmathieu@student.42mulhouse.fr>
2025-01-25 07:13:11 +0100

    testing

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -5,71 +5,76 @@ import pandas as pd
 
 
 def preprocessing(df: pd.DataFrame, target_name: str, keep_na: bool=False) -> pd.DataFrame:
-	data = df.select_dtypes(include='number')
-	classes = {c: i for i, c in enumerate(df[target_name].unique())}
-	data[target_name] = df[target_name].map(classes)
+    data = df.select_dtypes(include='number')
+    classes = {c: i for i, c in enumerate(df[target_name].unique())}
+    data[target_name] = df[target_name].map(classes)
 
-	if keep_na is False:
-		data.dropna(inplace=True)
-	else:
-		means = data.groupby(target_name).mean()
-		data = data.apply(lambda x: x.fillna(means.loc[x[target_name]]), axis=1)
-		data[target_name] = data[target_name].astype(int)
+    if keep_na is False:
+        data.dropna(inplace=True)
+    else:
+        means = data.groupby(target_name).mean()
+        data = data.apply(lambda x: x.fillna(means.loc[x[target_name]]), axis=1)
+        data[target_name] = data[target_name].astype(int)
 
-	return data
+    return data
 
 
 def standardize(lst: pd.Series) -> pd.Series:
-	mean = sum(lst) / len(lst)
-	std = (sum((abs(value - mean) ** 2) for value in lst) / (len(lst) - 1)) ** 0.5
-	return pd.Series(((lst - mean) / std), index=lst.index)
+    mean = sum(lst) / len(lst)
+    std = (sum((abs(value - mean) ** 2) for value in lst) / (len(lst) - 1)) ** 0.5
+    return pd.Series(((lst - mean) / std), index=lst.index)
 
 
 class LogisticRegression():
-	def __init__(self, features: pd.DataFrame, target: pd.Series, learning_rate: float = 0.01, epochs: int = 1000):
-		try:
-			if (features.shape[0] != target.shape[0]):
-				raise ValueError(
-					"LogisticRegression: Features and Target must be of the same shape.")
-			self.m, self.n_features = features.shape
-			self.n_classes = len(target.unique())
-			self.features = features
-			self.target = target
-			self.learning_rate = learning_rate
-			self.epochs = epochs
-			self.costs = []
-			self.bias = np.zeros(self.n_classes)
-			self.W = np.zeros((self.n_classes, self.n_features))
-		except Exception as e:
-			print(f'{type(e).__name__}: {e}')
-			return None
-		
-	def gradient_descent(self):
-		for _ in range(self.epochs):
-			logits = np.dot(self.features, self.W.T) + self.bias
-			pred = self.softmax(logits)
-			pred[range(self.m), self.target] -= 1
-			self.W -= self.learning_rate * (np.dot(pred.T, self.features) / self.m)
-			self.bias -= self.learning_rate * (np.sum(pred, axis=0) / self.m)
+    def __init__(self, features: pd.DataFrame, target: pd.Series, learning_rate: float = 0.01, epochs: int = 1000):
+        try:
+            if (features.shape[0] != target.shape[0]):
+                raise ValueError(
+                    "LogisticRegression: Features and Target must be of the same shape.")
+            self.m, self.n_features = features.shape
+            self.n_classes = len(target.unique())
+            self.features = features
+            self.target = target
+            self.learning_rate = learning_rate
+            self.epochs = epochs
+            self.costs = []
+            self.bias = np.random.random(self.n_classes)
+            self.W = np.random.random((self.n_classes, self.n_features))
+        except Exception as e:
+            print(f'{type(e).__name__}: {e}')
+            return None
 
+    def gradient_descent(self):
+        for _ in range(self.epochs):
+            logits = np.dot(self.features, self.W.T) + self.bias
+            pred = self.softmax(logits)
+            self.costs = 
+            pred[range(self.m), self.target] -= 1
+            self.W -= self.learning_rate * (np.dot(pred.T, self.features) / self.m)
+            self.bias -= self.learning_rate * (np.sum(pred, axis=0) / self.m)
 
-	def stochastic_gd(self):
-		for i in range(self.m):
-			logits = np.dot(self.features.iloc[[i]], self.W.T) + self.bias
-			pred = self.softmax(logits)
-			pred[[0], self.target.iloc[[i]]] -= 1
-			self.W -= self.learning_rate * np.dot(pred.T, self.features.iloc[[i]])
-			self.bias -= self.learning_rate * np.sum(pred, axis=0)
+    def compute_loss(self, y_true, y_pred):
+        y_zero_loss = y_true * np.log(y_pred + 1e-9)
+        y_one_loss = (1 - y_true) * np.log(1 - y_pred + 1e-9)
+        return -np.mean(y_zero_loss + y_one_loss)
 
-	def mini_batch_gd():
-		pass
+    def stochastic_gd(self):
+        for i in range(self.m):
+            logits = np.dot(self.features.iloc[[i]], self.W.T) + self.bias
+            pred = self.softmax(logits)
+            pred[[0], self.target.iloc[[i]]] -= 1
+            self.W -= self.learning_rate * np.dot(pred.T, self.features.iloc[[i]])
+            self.bias -= self.learning_rate * np.sum(pred, axis=0)
 
-	def softmax(self, logits):
-		z = np.exp(logits - np.max(logits, axis=1, keepdims=True))
-		return z / np.sum(z, axis=1, keepdims=True)
-	
-	def predict(self, data: pd.DataFrame):
-		logits = np.dot(data, self.W.T) + self.bias
-		probabilies = self.softmax(logits)
-		predictions = np.argmax(probabilies, axis=1)
-		return predictions
+    def mini_batch_gd():
+        pass
+
+    def softmax(self, logits):
+        z = np.exp(logits - np.max(logits, axis=1, keepdims=True))
+        return z / np.sum(z, axis=1, keepdims=True)
+        
+    def predict(self, data: pd.DataFrame):
+        logits = np.dot(data, self.W.T) + self.bias
+        probabilies = self.softmax(logits)
+        predictions = np.argmax(probabilies, axis=1)
+        return predictions
--- x.py
+++ x.py
@@ -7,39 +7,38 @@ from LogisticRegression import LogisticRegression
 def main():
 	df = pd.read_csv("dataset_train.csv")
 	df = preprocessing(df, "Hogwarts House", keep_na=True)
-	features = df.iloc[:0, 7:-1]
-	print(features)
-	exit()
+	features = df.iloc[:-1, 2:-1]
 	data = pd.DataFrame()
+
 	for f in features:
 		data.loc[:, f] = standardize(df[f])
 	target = df["Hogwarts House"]
 
 	print(data, '\n')
 
-	# lr = LogisticRegression(data, target)
-	# lr.gradient_descent()
-	# print(lr.W)
-	# print(lr.bias)
+	lr = LogisticRegression(data, target)
+	lr.gradient_descent()
+	print(lr.W)
+	print(lr.bias)
 
-	# pred = lr.predict(data)
-	# print('\n')
-	# print(pd.DataFrame(pred).value_counts())
-	# correct = pd.DataFrame(pred == target)
-	# print('\n')
-	# print(correct.value_counts())
-
-	l = LogisticRegression(data, target)
-	l.stochastic_gd()
-	print("weight:\n", l.W)
-	print(l.bias)
-
-	pred = l.predict(data)
+	pred = lr.predict(data)
 	print('\n')
 	print(pd.DataFrame(pred).value_counts())
 	correct = pd.DataFrame(pred == target)
 	print('\n')
 	print(correct.value_counts())
+
+	# l = LogisticRegression(data, target)
+	# l.stochastic_gd()
+	# print("weight:\n", l.W)
+	# print(l.bias)
+
+	# pred = l.predict(data)
+	# print('\n')
+	# print(pd.DataFrame(pred).value_counts())
+	# correct = pd.DataFrame(pred == target)
+	# print('\n')
+	# print(correct.value_counts())
 
 
 if __name__ == "__main__":
