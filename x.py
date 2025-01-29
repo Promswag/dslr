11a6cb42225288279e3356fc3fe390759590b2bf
Guillaume Baumgartner <promswag@gmail.com>
2025-01-29 17:55:25 +0100

    wip tests

--- x.py
+++ x.py
@@ -1,4 +1,4 @@
-from LogisticRegression import preprocessing
+from LogisticRegression import fill_na
 # from LogisticRegression import outliers_clamping_by_std
 from LogisticRegression import LogisticRegression
 from StandardScaler import StandardScaler
@@ -8,14 +8,18 @@ import time
 
 def main():
 	df = pd.read_csv("dataset_train.csv")
+	# df.dropna(inplace=True)
 	test_df = pd.read_csv("dataset_test.csv")
-	df, classes = preprocessing(df, "Hogwarts House", keep_na=True)
+	df = fill_na(df, "Hogwarts House")
 	target = df["Hogwarts House"]
 	features = [
 		'Defense Against the Dark Arts',
 		'Ancient Runes',
 		'Charms',
 	]
+
+	# kekeke = {c: i for i, c in enumerate(target.unique())}
+	# print(kekeke)
 	# features = [
 	# 	'Arithmancy',
 	# 	'Astronomy',
@@ -31,21 +35,32 @@ def main():
 	# 	'Charms',
 	# 	'Flying',
 	# ]
+	# return
 	scaler = StandardScaler()
 	data = scaler.fit_transform(df[features])
-	scaler.save_to_file()
-	scaled_test_df = scaler.transform(test_df[features])
+	# print(scaler.features)
+	# print(scaler.mean)
+	# print(scaler.std)
+
+	# scaler.from_file('scaler.csv')
+	# print(scaler.features)
+	# print(scaler.mean)
+	# print(scaler.std)
+	# return
+	# scaler.save_to_file()
+	# scaled_test_df = scaler.transform(test_df[features])
 	# scaler2 = StandardScaler()
 	# scaled_test_df = scaler2.fit_transform(test_df[features])
-	print(scaled_test_df)
+	# print(scaled_test_df)
 	# print(test_df[features])
 	# print(scaled_test_df)
 
 	lr = LogisticRegression(data, target)
-	now = time.time_ns()
+	# now = time.time_ns()
 	lr.gradient_descent()
-	then = time.time_ns()
-	print(f'{(then - now)/1e9:.2f}')
+	# then = time.time_ns()
+	# print(f'{(then - now)/1e9:.2f}')
+	# lr.plot_sigmoid(scaled_test_df[features], classes)
 	# print(lr.bias)
 	# lr.reset()
 
@@ -61,27 +76,28 @@ def main():
 	# print(f'{(then - now)/1e9:.2f}')
 	# lr.reset()
 
-	lr.save_to_file()
+	# lr.save_to_file()
 	# lr.mini_batch_gd()
 	# lr.stochastic_gd()
 	# print(lr.W)
 	# print(lr.bias)
 
-	pred = lr.predict(data[features])
-	print(classes)
-	print(pd.DataFrame(pred, columns=['House']).value_counts().sort_index())
-	correct = pd.DataFrame(pred == target)
-	print(correct.value_counts())
-
-	weights = pd.read_csv('weights.csv')
-	pred = lr.predict_from_weights(data[features], weights, classes, to_file=True)
+	# pred = lr.predict(data[features])
 	# print(classes)
-	# print(pd.DataFrame(pred, columns=['House']).value_counts().sort_index())
-	correct = pd.DataFrame(pred == target)
-	print(correct.value_counts())
+	# print(pred.value_counts())
+	# print((pred == target).value_counts())
 
-	pred = lr.predict_from_weights(scaled_test_df[features], weights, classes)
-	print(pd.DataFrame(pred).value_counts().sort_index())
+	# lr.load_weights('weights.csv')
+	pred = lr.predict_from_weights(data[features], 'weights.csv')
+	# print(classes)
+	print(pred)
+	print(pred.value_counts())
+	correct = (pred == target).value_counts()
+	print(correct)
+	print(correct.iloc[0] / len(pred))
+
+	# pred = lr.predict_from_weights(scaled_test_df[features], weights)
+	# print(pd.DataFrame(pred).value_counts().sort_index())
 
 	# print(pred)
 
