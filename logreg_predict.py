a437ecfd6d9fb21e0cce32ae87f6334d8de56283
Guillaume Baumgartner <promswag@gmail.com>
2025-01-30 19:22:19 +0100

    logreg_predict

---
+++ logreg_predict.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import sys
+from LogisticRegression import LogisticRegression
+from StandardScaler import StandardScaler
+
+def main():
+	try:
+		if len(sys.argv) < 2 or len(sys.argv) > 3: 
+			raise Exception(f"Usage: {sys.argv[0]} <dataset.csv>")
+		
+		for file in sys.argv[1:]:
+			if file[-4:] != ".csv":
+				raise Exception(f"Wrong file format for {file}, expected: .csv")
+		
+		weights = pd.read_csv('weights.csv')
+		features = [f for f in weights.columns[2:]]
+
+		df = pd.read_csv(sys.argv[-1], index_col='Index')[features]
+
+		scaler = StandardScaler().from_file()
+		df[features] = scaler.transform(df[features])
+		
+		pred = LogisticRegression.predictXD(df[features], weights, to_file='houses.csv')
+		
+	except Exception as e:
+		print(f'{type(e).__name__} : {e}')
+
+
+if __name__ == "__main__":
+	main()
+
