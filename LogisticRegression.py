f6a66818874e31054c0563848b39dfc3dad61471
jmathieu <jmathieu@Jey.>
2025-01-30 12:12:51 +0100

    graphs

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -39,6 +39,7 @@ class LogisticRegression():
             self.learning_rate = learning_rate
             self.epochs = epochs
             self.losses = []
+            self.pred = []
             self.bias = np.random.random(self.n_classes)
             self.W = np.random.random((self.n_classes, self.n_features))
         except Exception as e:
@@ -48,11 +49,11 @@ class LogisticRegression():
     def gradient_descent(self):
         for _ in range(self.epochs):
             logits = np.dot(self.features, self.W.T) + self.bias
-            pred = self.softmax(logits)
-            loss_pred = pred.copy()
-            pred[range(self.m), self.target] -= 1
-            self.W -= self.learning_rate * (np.dot(pred.T, self.features) / self.m)
-            self.bias -= self.learning_rate * (np.sum(pred, axis=0) / self.m)
+            self.pred = self.softmax(logits)
+            loss_pred = self.pred.copy()
+            self.pred[range(self.m), self.target] -= 1
+            self.W -= self.learning_rate * (np.dot(self.pred.T, self.features) / self.m)
+            self.bias -= self.learning_rate * (np.sum(self.pred, axis=0) / self.m)
             self.losses.append(self.compute_loss(loss_pred))
 
         plt.plot(range(len(self.losses)), self.losses, label="Loss Curve")
@@ -62,47 +63,21 @@ class LogisticRegression():
         plt.legend()
         plt.savefig('1')
 
-        sample_input = self.features.iloc[0:-1]  # Take the first sample
-        probabilities = self.softmax(np.dot(sample_input, self.W.T) + self.bias)
-        plt.bar(range(len(probabilities[0])), probabilities[0])
-        plt.xlabel("Class")
-        plt.ylabel("Probability")
-        plt.title("Class Probability Distribution for a Sample Input")
-        plt.savefig('2')
-
-        # x_min, x_max = self.features.iloc[:, 0].min() - 1, self.features.iloc[:, 0].max() + 1
-        # y_min, y_max = self.features.iloc[:, 1].min() - 1, self.features.iloc[:, 1].max() + 1
-        # xx, yy = np.meshgrid(np.linspace(x_min, x_max, 100),
-        #                     np.linspace(y_min, y_max, 100))
-        # Z = self.predict(pd.DataFrame(np.c_[xx.ravel(), yy.ravel()]))
-        # Z = Z.reshape(xx.shape)
-        # plt.contourf(xx, yy, Z, alpha=0.3)
-        # plt.scatter(self.features.iloc[:, 0], self.features.iloc[:, 1], c=self.target, edgecolor='k')
-        # plt.xlabel("Feature 1")
-        # plt.ylabel("Feature 2")
-        # plt.title("Decision Boundary")
-        # plt.savefig('3')
-
         y_true = self.target
         y_pred = self.predict(self.features)
         cm = confusion_matrix(y_true, y_pred)
         disp = ConfusionMatrixDisplay(confusion_matrix=cm)
         disp.plot(cmap=plt.cm.Blues)
         plt.title("Confusion Matrix")
-        plt.savefig('4')
+        plt.savefig('2')
 
-    x = np.linspace(-10, 10, 100)
-    logits = np.array([x, -x, x/2, -x/2]).T
-    softmax_values = np.exp(logits) / np.sum(np.exp(logits), axis=1, keepdims=True)
-    plt.plot(x, softmax_values[:, 0], label="Class 1")
-    plt.plot(x, softmax_values[:, 1], label="Class 2")
-    plt.plot(x, softmax_values[:, 2], label="Class 3")
-    plt.plot(x, softmax_values[:, 3], label="Class 4")
-    plt.xlabel("Feature Value")
-    plt.ylabel("Probability")
-    plt.title("Softmax Output for 4 Classes")
-    plt.legend()
-    plt.savefig('5')
+        plt.figure(figsize=(8, 5))
+        plt.bar(range(len(self.pred[0])), self.pred[0])
+        plt.xlabel("Class")
+        plt.ylabel("Probability")
+        plt.title("Softmax Output for a Sample Input")
+        plt.xticks(range(len(self.pred[0])), labels=[f'Class {i}' for i in range(len(self.pred[0]))])
+        plt.savefig('3')
 
     def compute_loss(self, pred):
         epsilon = 1e-15
